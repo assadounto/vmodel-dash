45e2b54ba8e52a3554ab9d9c00a171af73149441
“rich” <adukyerer@gmail.com>
2023-11-08 09:45:59 +0000

    add skeleton

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import "./assets/stylesheets/App.scss"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 
 function App() {
+ 
+  const isAuthenticated= true
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>login</div>} />
+        <Route
+          path="/admin/*"
+          element={
+            <PrivateRoute
+            element={
+              <AdminLayout>
+                <Routes>
+                  {/* <Route index element={<Dashboard/>} />
+                  <Route path="/vendors" element={<VendorPage/>} />
+                  <Route path="/reviews" element={<ReviewsPage/>} />
+                  <Route path="/users" element={<UsersPage/>} /> */}
+                </Routes>
+              </AdminLayout>
+            }
+            isAuthenticated={isAuthenticated}
+          />
+          }
+        />
+      </Routes>
+
+     
+    </BrowserRouter>
   );
 }
 
