f3cde4d285d3e741bd822cdde0a4f82bc260e01b
“rich” <adukyerer@gmail.com>
2023-11-09 10:27:49 +0000

    Add login

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,17 +1,22 @@
 import React, { useState } from "react";
-
+import { useNavigate } from "react-router-dom";
 const LoginForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
+  const navigate = useNavigate();
   const handleSubmit = (e) => {
+
     e.preventDefault();
     // Add your authentication logic here, e.g., make an API request to log in the user.
+    // If authentication is successful, navigate to admin/dashboard
     console.log("Login button clicked");
+
+    // Assuming authentication is successful, navigate to admin/dashboard
+   navigate("/admin/dashboard");
   };
 
   return (
-    <form action="#">
+    <form onSubmit={handleSubmit}>
      
       <div className="login-cont"> 
       <div className="login-label">LOGIN</div>
@@ -34,7 +39,11 @@ const LoginForm = () => {
         placeholder="Enter Username"
         required
       />
+            <button type="submit" className="login-button">
+  Login
+</button>
       </div>
+
     </form>
   );
 };
