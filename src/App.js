0dcf0ecc3c2874938080bc1884e01a7939b3d1aa
“rich” <adukyerer@gmail.com>
2023-11-13 10:47:20 +0000

    Add new

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-      
+      <Route path="/login" element={<LoginPage/>} />
         <Route
           path="/admin/*"
           element={
